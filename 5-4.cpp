019cfb983d5abfaee0968e0c80bcd0c3d4f8e8f9
SanghyeonKim <44115202+limetimeline@users.noreply.github.com>
2022-09-15 11:46:41 +0900

    Create 5-4.cpp

---
+++ 5-4.cpp
@@ -0,0 +1,42 @@
+#include <GL/glut.h>
+#include <stdio.h>
+
+void MyDisplay() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glViewport(0, 0, 300, 300);
+	glRotatef(45, 1.0, 1.0, 1.0);
+	glColor3f(1.0, 1.0, 1.0);
+	glutWireCube(0.5);
+
+	glRotatef(45, 1.0, 1.0, 1.0);
+	glViewport(300, 300, 300, 300);
+	glColor3f(1.0, 1.0, 0.0);
+	glutWireCone(0.5,1,10,10);
+
+	glRotatef(45, 1.0, 1.0, 1.0);
+	glViewport(0, 300, 300, 300);
+	glColor3f(1.0, 0.0, 0.0);
+	glutWireSphere(0.5, 10, 10);
+
+	glRotatef(200, 1.0, 1.0, 1.0);
+	glViewport(300, 0, 300, 300);
+	glColor3f(0.0, 1.0, 0.0);
+	glutWireTorus(0.2, 0.4, 10 ,10);
+
+
+
+
+	glFlush();
+}
+
+int main() {
+	glutInitWindowSize(600, 600);
+	glutCreateWindow("김상현");
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+	glutDisplayFunc(MyDisplay);
+	glutMainLoop();
+	return 0;
+}
