963785d9f0f9c0a7da696b7a46d21f8d07019d28
SanghyeonKim <44115202+limetimeline@users.noreply.github.com>
2022-10-19 22:13:08 +0900

    Update PreventDistortion.cpp

--- code/PreventDistortion.cpp
+++ code/PreventDistortion.cpp
@@ -37,22 +37,52 @@ void DrawScene() {
 void MyDisplay() {
 	glClear(GL_COLOR_BUFFER_BIT);
 	glColor3f(1.0, 1.0, 1.0);
-	glViewport(0, 0, Width / 2, Height / 2);
+
+	// 순서 : glMatrixMode(GL_PROJECTION) -> glLoadIdentity() -> gluPerspective() -> glMatrixMode(GL_MODELVIEW)
+	// 물체, 카메라 위치 및 방향에 대한 기하 변환 -> glMatrixMode(GL_PROJECTION)
+
+	/* glViewport(left, bottom, width, height) */
+	glViewport(0, 0, Width / 2, Height / 2); // 3사분면
+	glMatrixMode(GL_PROJECTION); // 촬영 방법 및 렌즈 선택에 대한 기하 변환
 	glPushMatrix();
-		gluLookAt(0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-		DrawScene();
+		glLoadIdentity();
+		/* gluPerspective(fov, aspect, near, far) */
+		gluPerspective(30, (GLdouble)Width / (GLdouble)Height, 1.0, 50.0); // 종횡비를 뷰포트와 일치. (왜곡 방지)
+		glMatrixMode(GL_MODELVIEW); // 물체, 카메라 위치 및 방향에 대한 기하 변환
+		glPushMatrix();
+			gluLookAt(0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+			DrawScene();
+		glPopMatrix();
+		glMatrixMode(GL_PROJECTION);
 	glPopMatrix();
-	glViewport(Width / 2, 0, Width / 2, Height / 2);
+
+	glViewport(Width / 2, 0, Width / 2, Height / 2); // 4사분면
+	glMatrixMode(GL_PROJECTION);
 	glPushMatrix();
-		gluLookAt(1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-		DrawScene();
+		glLoadIdentity();
+		gluPerspective(30, (GLdouble)Width / (GLdouble)Height, 1.0, 50.0);
+		glMatrixMode(GL_MODELVIEW);
+		glPushMatrix();
+		gluLookAt(5.0, 0.0, -0.5, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+			DrawScene();
+		glPopMatrix();
+		glMatrixMode(GL_PROJECTION);
 	glPopMatrix();
-	glViewport(0, Height / 2, Width / 2, Height / 2);
+
+	glViewport(0, Height / 2, Width / 2, Height / 2); // 2사분면
+	glMatrixMode(GL_PROJECTION);
 	glPushMatrix();
-		gluLookAt(0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0);
-		DrawScene();
+		glLoadIdentity();
+		gluPerspective(40, (GLdouble)Width / (GLdouble)Height, 1.0, 50.0);
+		glMatrixMode(GL_MODELVIEW);
+		glPushMatrix();
+			gluLookAt(0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0);
+			DrawScene();
+		glPopMatrix();
+		glMatrixMode(GL_PROJECTION);
 	glPopMatrix();
-	glViewport(Width / 2, Height / 2, Width / 2, Height / 2);
+
+	glViewport(Width / 2, Height / 2, Width / 2, Height / 2);// 1사분면
 	glMatrixMode(GL_PROJECTION);
 	glPushMatrix();
 		glLoadIdentity();
@@ -65,6 +95,7 @@ void MyDisplay() {
 		glPopMatrix();
 		glMatrixMode(GL_PROJECTION);
 	glPopMatrix();
+
 	glFlush();
 }
 
