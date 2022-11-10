e41222d6dca6a32164f67689ae3fe51a9aa12744
SanghyeonKim <44115202+limetimeline@users.noreply.github.com>
2022-11-11 00:56:24 +0900

    Update ClipPlane.cpp

--- code/ClipPlane.cpp
+++ code/ClipPlane.cpp
@@ -5,33 +5,45 @@
 
 void MyDisplay() {
 	GLdouble eqn1[4] = { 0.0, 1.0, 0.0, 0.0 };
-	GLdouble eqn2[4] = { 1.0, 0.0, 0.0, 0.0 };
+	GLdouble eqn2[4] = { -1.0, 0.0, 0.0, 0.0 };
 	GLdouble eqn3[4] = { 1.0, 1.0, 0.0, 0.0 };
-	GLdouble eqn4[4] = { 1.0, 0.0, 0.0, 0.5 };
-	GLdouble eqn[4] = { 0.0, -1.0, 0.0, 0.0 };
+	GLdouble eqn4[4] = { 1.0, 1.0, 0.0, 0.0 };
 
 	glMatrixMode(GL_MODELVIEW);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glColor3f(0.0, 1.0, 0.0);
 
 	glTranslatef(0.0, 0.0, -5.0);
-	
-	// 1번
-	//glClipPlane(GL_CLIP_PLANE0, eqn1);
-	//glEnable(GL_CLIP_PLANE0);
 
-	// 2번
+
+	// 1번
 	glClipPlane(GL_CLIP_PLANE0, eqn1);
 	glEnable(GL_CLIP_PLANE0);
+
+	// 2번
+	//glClipPlane(GL_CLIP_PLANE0, eqn2);
+	//glEnable(GL_CLIP_PLANE0);
+
+	// 3번
+	//glClipPlane(GL_CLIP_PLANE0, eqn1);
+	//glEnable(GL_CLIP_PLANE0);
 
 	//glClipPlane(GL_CLIP_PLANE1, eqn2);
 	//glEnable(GL_CLIP_PLANE1);
 
-	//glClipPlane(GL_CLIP_PLANE0, eqn);
+	// 4번
+	//glClipPlane(GL_CLIP_PLANE0, eqn3);
 	//glEnable(GL_CLIP_PLANE0);
 
-	glRotatef(90.0, 1.0, 0.0, 0.0);
+	// 5번
+	//glClipPlane(GL_CLIP_PLANE0, eqn1);
+	//glEnable(GL_CLIP_PLANE0);
 
+	//glClipPlane(GL_CLIP_PLANE1, eqn4);
+	//glEnable(GL_CLIP_PLANE1);
+
+	glRotatef(90.0, 1.0, 0.0, 0.0);
+	
 	glutWireSphere(1.0, 20, 16);
 	glFlush();
 }
@@ -48,7 +60,7 @@ int main() {
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA | GLUT_DEPTH);
 	glutInitWindowSize(500, 500);
 	glutInitWindowPosition(500, 400);
-	glutCreateWindow("Clip Plane");
+	glutCreateWindow("김상현");
 	glClearColor(0.0, 0.0, 0.0, 0.0);
 	glutDisplayFunc(MyDisplay);
 	glutReshapeFunc(MyReshape);
